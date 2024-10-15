eb1dd71b9fcddeb0d868b89df237e346b9dc01fb
davi paluch <davipaluch@icloud.com>
2024-10-14 23:51:29 -0300

    feat: starting next app and Sidebar

---
+++ website-homepage-example/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="p-10 bg-slate-50 text-slate-900 flex">
+      <p className="text-4xl font-bold text-center flex-1">
+        Home Page
+      </p>
+      <p></p>
+    </div>
+  );
+}
